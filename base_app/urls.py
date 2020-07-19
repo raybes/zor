d00dba6f06589d545327f88c74dc680c4a04702e
raybes <wrjones311@gmail.com>
2020-07-18 19:19:13 -0700

    Initial Commit

---
+++ base_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomePageView, ContactPageView
+
+urlpatterns = [
+	path('', HomePageView.as_view(), name='home'),
+	path('contact/', ContactPageView.as_view(), name = 'contact'),
+]
