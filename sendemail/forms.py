ab6c9d195e54c1352b92139839b147503e16cd36
raybes <wrjones311@gmail.com>
2020-07-21 08:27:56 -0700

    more sendgrid smtp attempts

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	user_email = forms.EmailField(required=True)
+	from_email = forms.EmailField(required=True)
 	subject = forms.CharField(required=True)
 	message = forms.CharField(widget=forms.Textarea, required=True)
