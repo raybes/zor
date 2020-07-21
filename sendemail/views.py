56f0320df400d7c1c92aed0ad6d9623a5a605989
raybes <wrjones311@gmail.com>
2020-07-21 08:10:29 -0700

    more email sender identity issue attempted fixes

--- sendemail/views.py
+++ sendemail/views.py
@@ -3,7 +3,6 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactForm
-from django.shortcuts import render
 
 # Create your views here.
 
